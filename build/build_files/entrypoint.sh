bbb81d3482ff4b52d88391134b1757158a8f7fd1
Hendrik Leidinger <hendrik.leidinger@nextcloud.com>
2026-01-21 17:13:51 -0800

    fixed font creation issue. Most functionality seems to work now

--- build/build_files/entrypoint.sh
+++ build/build_files/entrypoint.sh
@@ -6,6 +6,8 @@ service redis-server start
 service nginx start
 
 #tail -f /dev/null
+/usr/bin/documentserver-generate-allfonts.sh
+
 NODE_ENV=production-linux NODE_CONFIG_DIR=/etc/onlyoffice/documentserver NODE_DISABLE_COLORS=1 APPLICATION_NAME=ONLYOFFICE /var/www/onlyoffice/documentserver/server/FileConverter/converter &
 NODE_ENV=production-linux NODE_CONFIG_DIR=/etc/onlyoffice/documentserver NODE_DISABLE_COLORS=1 APPLICATION_NAME=ONLYOFFICE /var/www/onlyoffice/documentserver/server/DocService/docservice
 
