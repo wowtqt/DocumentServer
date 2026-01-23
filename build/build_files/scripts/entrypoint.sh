3c85a884fed8e7644b0f29eb77634af98e16ab3b
Hendrik Leidinger <hendrik.leidinger@nextcloud.com>
2026-01-23 11:01:47 -0800

    fix logs piped to correct position

--- build/build_files/scripts/entrypoint.sh
+++ build/build_files/scripts/entrypoint.sh
@@ -8,6 +8,6 @@ service nginx start
 #tail -f /dev/null
 /usr/bin/documentserver-generate-allfonts.sh
 
-NODE_ENV=development-linux NODE_CONFIG_DIR=/etc/onlyoffice/documentserver NODE_DISABLE_COLORS=1 APPLICATION_NAME=ONLYOFFICE /var/www/onlyoffice/documentserver/server/FileConverter/converter &
-NODE_ENV=development-linux NODE_CONFIG_DIR=/etc/onlyoffice/documentserver NODE_DISABLE_COLORS=1 APPLICATION_NAME=ONLYOFFICE /var/www/onlyoffice/documentserver/server/DocService/docservice
+NODE_ENV=development-linux NODE_CONFIG_DIR=/etc/onlyoffice/documentserver NODE_DISABLE_COLORS=1 APPLICATION_NAME=ONLYOFFICE LD_LIBRARY_PATH=/var/www/onlyoffice/documentserver/server/FileConverter/bin /bin/sh -c 'exec /var/www/onlyoffice/documentserver/server/FileConverter/converter 2>&1 | tee -a /var/log/onlyoffice/documentserver/converter/out.log' &
+NODE_ENV=development-linux NODE_CONFIG_DIR=/etc/onlyoffice/documentserver NODE_DISABLE_COLORS=1 APPLICATION_NAME=ONLYOFFICE /bin/sh -c 'exec /var/www/onlyoffice/documentserver/server/DocService/docservice 2>&1 | tee -a /var/log/onlyoffice/documentserver/docservice/out.log'
 
